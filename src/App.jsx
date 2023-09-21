9ff302d39df2a19b07bc67e7dce703679db662fe
Nahid Parvez <nahid.parvez@bjitacademy.com>
2023-09-21 16:44:28 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Header from "./components/header"
+
+function App() {
+
+  console.log("Rendering form app.jsx file")
+
+
+  return (
+    <div>
+      <Header />
+      <div>
+        <h1>React Introduction</h1>
+        <p>Helllo, we are learning React</p>
+        <p>Some basic components</p>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App
